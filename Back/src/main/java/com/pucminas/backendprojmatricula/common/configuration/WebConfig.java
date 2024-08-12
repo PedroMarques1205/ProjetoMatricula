9d2d5c9dac0d6e858c6e2fbc46f7e07fc5c30ae1
Pedro Marques <pedrophmo1205@gmail.com>
2024-08-12 13:55:43 -0300

    add congi cors

---
+++ Back/src/main/java/com/pucminas/backendprojmatricula/common/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.pucminas.backendprojmatricula.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
