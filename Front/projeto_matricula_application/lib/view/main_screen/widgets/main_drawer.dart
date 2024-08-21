48de741109fe9e639223e96886361511375370bc
julipana <julicunha04@gmail.com>
2024-08-21 16:19:11 -0300

    verificação dos botões do drawer

--- Front/projeto_matricula_application/lib/view/main_screen/widgets/main_drawer.dart
+++ Front/projeto_matricula_application/lib/view/main_screen/widgets/main_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:projeto_matricula_application/design/colors/project_colors.dart';
 import 'package:projeto_matricula_application/domain/context/context.dart';
+import 'package:projeto_matricula_application/domain/login/dtos/user_dto.dart';
 import 'package:projeto_matricula_application/view/shared/button_widget.dart';
 
 class DrawerWidget extends StatelessWidget {
@@ -14,9 +15,7 @@ class DrawerWidget extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
-            decoration: const BoxDecoration(
-              color: ProjectColors.primaryLight
-            ),
+            decoration: const BoxDecoration(color: ProjectColors.primaryLight),
             child: Center(
               child: Column(
                 children: [
@@ -47,48 +46,51 @@ class DrawerWidget extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Align(
-              alignment: Alignment.center,
-              child: ButtonWidget(
-                icon: HeroIcons.academicCap,
-                text: 'Matrícula',
-                width: 250,
-                onPressed: () {},
-                backgroundColor: ProjectColors.buttonColor,
-                textColor: Colors.grey[600],
+          if (Context.current.tipoAcesso == UserTypeEnum.Aluno)
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Align(
+                alignment: Alignment.center,
+                child: ButtonWidget(
+                  icon: HeroIcons.academicCap,
+                  text: 'Matrícula',
+                  width: 250,
+                  onPressed: () {},
+                  backgroundColor: ProjectColors.buttonColor,
+                  textColor: Colors.grey[600],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Align(
-              alignment: Alignment.center,
-              child: ButtonWidget(
-                icon: HeroIcons.currencyDollar,
-                text: 'Financeiro',
-                width: 250,
-                onPressed: () {},
-                backgroundColor: ProjectColors.buttonColor,
-                textColor: Colors.grey[600],
+          if (Context.current.tipoAcesso == UserTypeEnum.Aluno)
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Align(
+                alignment: Alignment.center,
+                child: ButtonWidget(
+                  icon: HeroIcons.currencyDollar,
+                  text: 'Financeiro',
+                  width: 250,
+                  onPressed: () {},
+                  backgroundColor: ProjectColors.buttonColor,
+                  textColor: Colors.grey[600],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Align(
-              alignment: Alignment.center,
-              child: ButtonWidget(
-                icon: HeroIcons.bookOpen,
-                text: 'Minhas Matérias',
-                width: 250,
-                onPressed: () {},
-                backgroundColor: ProjectColors.buttonColor,
-                textColor: Colors.grey[600],
+          if (Context.current.tipoAcesso == UserTypeEnum.Aluno)
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Align(
+                alignment: Alignment.center,
+                child: ButtonWidget(
+                  icon: HeroIcons.bookOpen,
+                  text: 'Minhas Matérias',
+                  width: 250,
+                  onPressed: () {},
+                  backgroundColor: ProjectColors.buttonColor,
+                  textColor: Colors.grey[600],
+                ),
               ),
             ),
-          ),
         ],
       ),
     );
